15f2b409bd348c7c601fc0dc238e698259f94210
queue <angeltrisix@gmail.com>
2025-03-18 15:21:02 -0500

    Update MoreLocales.cs

--- MoreLocales.cs
+++ MoreLocales.cs
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) <Year> <Your Name/Organization>
+ * Copyright (C) 2025 qAngel
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
